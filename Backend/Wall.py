2776969e57a0ee346ec8ee524f1cd12202c20dad
Anushanga2001 <anushanga2001@gmail.com>
2024-08-11 23:17:13 +0530

    commit-3

---
+++ Backend/Wall.py
@@ -0,0 +1,13 @@
+# get the length
+age = 36
+
+# When dealing with variables
+txt = f"Department of industrial management {age}"
+print(txt)
+print(bool(txt))
+print(len(txt)) 
+print(txt[2])
+
+# replace letters
+a = "Hello, World!"
+print(a.replace("H", "A"))
