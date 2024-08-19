7e52d55dbc26e789070567d41d5a9c0c2e8171c4
Anushanga2001 <anushanga2001@gmail.com>
2024-08-20 00:04:42 +0530

    commit-02

--- Backend/app.py
+++ Backend/app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template
 
 # This is test comment
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('template/Cal.html')  # Render the Cal.html file
+    return render_template('Cal.html')  # Render the Cal.html file
 
 @app.route('/test')
 def test():
-    return render_template('template/Test.html')  # Render the Test.html file
+    return render_template('Test.html')  # Render the Test.html file
 
 if __name__ == '__main__':
     app.run(debug=True)
